1ef78aa7153ef7b856f37940464775a8e716e21c
djumanov <diyorbekjumanov1120@gmail.com>
2023-08-26 14:42:45 +0500

    __name__

--- echobot.py
+++ echobot.py
@@ -25,32 +25,37 @@ import os
 # get token from environment variable
 TOKEN = os.environ.get('TOKEN')
 
-# create updater object
-updater = Updater(token=TOKEN)
 
-# get dispatcher from updater
-dispatcher = updater.dispatcher
+def main():
+    # create updater object
+    updater = Updater(token=TOKEN)
+    
+    # get dispatcher from updater
+    dispatcher = updater.dispatcher
+    
+    # add start handler to dispatcher
+    dispatcher.add_handler(handler=CommandHandler(command=('start', 'boshlash'), callback=start))
+    
+    # add echo handler to dispatcher
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.text, callback=echo_text))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.photo, callback=echo_photo))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.sticker, callback=echo_sticker))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.voice, callback=echo_voice))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.video, callback=echo_video))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.document, callback=echo_document))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.location, callback=echo_location))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.contact, callback=echo_contact))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.animation, callback=echo_animation))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.audio, callback=echo_audio))
+    dispatcher.add_handler(handler=MessageHandler(filters=Filters.video_note, callback=echo_video_note))
+    
+    
+    # start polling
+    updater.start_polling()
+    
+    # stop polling after 60 seconds
+    updater.idle()
+    
 
-# add start handler to dispatcher
-dispatcher.add_handler(handler=CommandHandler(command=('start', 'boshlash'), callback=start))
-
-# add echo handler to dispatcher
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.text, callback=echo_text))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.photo, callback=echo_photo))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.sticker, callback=echo_sticker))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.voice, callback=echo_voice))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.video, callback=echo_video))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.document, callback=echo_document))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.location, callback=echo_location))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.contact, callback=echo_contact))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.animation, callback=echo_animation))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.audio, callback=echo_audio))
-dispatcher.add_handler(handler=MessageHandler(filters=Filters.video_note, callback=echo_video_note))
-
-
-# start polling
-updater.start_polling()
-
-# stop polling after 60 seconds
-updater.idle()
-
+if __name__ == '__main__':
+    main()
